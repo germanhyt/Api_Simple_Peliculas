63268bf250aa41cd06eceb8a3eedd9aebc7920e2
germanhyt <85075441+germanhyt@users.noreply.github.com>
2023-03-22 22:43:53 -0500

    Add change

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
-<?php 
- include "api_peliculas.php";
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 
- $api=new Api_peliculas();
 
- if(isset($_GET['id'])){
-    $id=$_GET['id'];
-    if(is_numeric($id)){
+
+include "api_peliculas.php";
+
+$api = new Api_peliculas();
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    if (is_numeric($id)) {
         $api->getById($id);
-    }else{
+    } else {
         $api->error("El id es incorrecto");
     }
- }else if(isset($_GET['delete'])){
-    $delete_id=$_GET['delete'];
-    if(is_numeric($delete_id)){
+} else if (isset($_GET['delete'])) {
+    $delete_id = $_GET['delete'];
+    if (is_numeric($delete_id)) {
         $api->deletePeli($delete_id);
-    }else{
+    } else {
         $api->error("El id del delete es incorrecto");
     }
- }else{
+} else {
     $api->getAll();
- }
-
-?>
+}
